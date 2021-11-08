f5fdd743cbdb4eb8e40f84e09145509751615bcc
sfwat <m.abdelrahman1994@gmail.com>
2021-11-08 20:54:45 +0200

    models V1

--- so2alApp/models.py
+++ so2alApp/models.py
@@ -4,9 +4,18 @@ from django.db import models
 
 
 class Submitter(models.Model):
+    MALE = "M"
+    FEMALE = "F"
+    GENDER_CHOICES = [
+        (MALE, "M"),
+        (FEMALE, "F"),
+    ]
+
     name = models.CharField(max_length=100, unique=True)
+    nationality = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=100, blank=True)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
     def __str__(self):
         return self.name
@@ -15,6 +24,25 @@ class Submitter(models.Model):
 class Question(models.Model):
     question = models.CharField(max_length=100)
 
+    def already_submitted(self, submitter_id):
+        """
+        Return True if user already submitted
+        """
+        answer = self.answer.filter(submitter=submitter_id)
+        if answer:
+            return True
+        return False
+
+    def answer_count(self):
+        return self.answer.count()
+
+    def choice_percent(self):
+        answer_count = self.answer_count()
+        result = {}
+        for choice in self.choice.all():
+            result[choice.choice_text] = (choice.answer_count() / answer_count) * 100
+        return result
+
     def __str__(self):
         return self.question
 
@@ -22,6 +50,9 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choice')
     choice_text = models.CharField(max_length=100)
+
+    def answer_count(self):
+        return self.answer.count()
 
     def __str__(self):
         return self.choice_text
@@ -34,5 +65,3 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"{self.submitter} {self.question}: {self.choice}"
-
-
